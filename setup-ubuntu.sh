ea11689c0313e1e5cb6946222353b910622490cf
cam <cameronmcorist@gmail.com>
2024-11-12 23:26:18 +1100

    install more stuff

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Update package list and install Ansible
+# Update package list and install Ansible and Git
 sudo apt update
 sudo apt install -y ansible git
 
-# Clone the camsible repository
-git clone -b start-from-scratch https://github.com/c-mco/camsible.git
+# Change to the home directory
+cd ~
 
-# Change directory to the cloned repository
-cd camsible
+# Check if the camsible repository already exists
+if [ -d "camsible" ]; then
+  # If it exists, pull the latest changes
+  cd camsible
+  git pull
+else
+  # If it doesn't exist, clone the repository
+  git clone -b start-from-scratch https://github.com/c-mco/camsible.git
+  cd camsible
+fi
 
 # Run the camsible playbook
 ansible-playbook camsible.yml
