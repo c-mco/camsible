3ff9e98739c2763ac44e5188a690343b6595fcd9
cam <cameronmcorist@gmail.com>
2024-11-12 23:08:59 +1100

    branch

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y ansible git
 
 # Clone the camsible repository
-git clone https://github.com/c-mco/camsible.git
+git clone -b start-from-scratch https://github.com/c-mco/camsible.git
 
 # Change directory to the cloned repository
 cd camsible
