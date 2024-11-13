a1cd31128745bf964b8c7eff8b594817f5b31a30
cam <cam@camcamcam.cam>
2024-11-13 20:39:16 +1100

    password prompt

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -9,17 +9,21 @@ cd ~
 # Check if the camsible repository already exists
 if [ -d "camsible" ]; then
   # If it exists, pull the latest changes
+  echo "Camsible repository already exists. Pulling the latest changes..."
   cd camsible
   git pull
 else
   # If it doesn't exist, clone the repository
+  echo "Cloning the camsible repository..."
   git clone -b start-from-scratch https://github.com/c-mco/camsible.git
   cd camsible
 fi
 
 # Prompt for the Ansible Vault password
 read -sp 'Enter Ansible Vault password: ' VAULT_PASS
-echo
 
 # Run the camsible playbook
+echo "Running the camsible playbook..."
 ansible-playbook camsible.yml --vault-password-file <(echo "$VAULT_PASS")
+
+echo "Setup complete."
